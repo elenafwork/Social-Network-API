8a756d4197079c697c51ad02e630c47cf90e66aa
elenaf007 <96072892+elenaf007@users.noreply.github.com>
2023-09-02 16:40:45 -0500

    repo schema and models

---
+++ models/Reaction.js
@@ -0,0 +1,37 @@
+const {Schema}=require('mongoose');
+
+//schema only
+const reactionSchema=new Schema(
+    {
+        reactionId: {
+            type:  Schema.Types.ObjectId,
+            default: new ObjectId
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username:{
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => {
+                if (date) return date.toISOString().split("T") [0];
+              }, 
+        }
+    },
+    {
+        timestamps: true,
+        toJSON: {
+          virtuals: true,
+        },
+        id: false,
+    },
+
+);
+
+module.exports=Reaction;
---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require("mongoose");
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+const userSchema = new Schema(
+{
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [validateEmail, 'Please fill a valid email address'],
+  },
+  thoughts: [
+    {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }
+  ],
+  friends: [
+    {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+  ]
+},
+{
+    toJSON: {
+        virtuals: true,
+        trim: true,
+        
+    },
+    id: false
+},
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+})
+);
+const User = model('user', userSchema);
+module.exports = User;
